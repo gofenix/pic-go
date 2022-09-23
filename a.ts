a0603619088e5712ef297fb2b65bd8a679693b88
Fenix <zhuzhenfeng1993@hotmail.com>
2022-09-23 14:07:18 +0000

    add data

--- a.ts
+++ a.ts
@@ -1,9 +1,9 @@
 import axiod from "https://deno.land/x/axiod/mod.ts";
-import { print } from "./base.js";
+
 
 
 const { data } = await axiod<{ delay: string }>(
   "https://postman-echo.com/delay/2"
 );
 
-print(data)
+console.log(data)
